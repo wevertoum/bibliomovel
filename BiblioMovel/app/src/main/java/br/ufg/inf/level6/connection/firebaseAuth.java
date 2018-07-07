12860232a5700f1663f88accaf76e6c62ea32f35
Victor <victor.stillo@gmail.com>
2018-07-07 15:18:43 -0300

    Create connection e internet permission

---
+++ BiblioMovel/app/src/main/java/br/ufg/inf/level6/connection/firebaseAuth.java
@@ -0,0 +1,10 @@
+package br.ufg.inf.level6.connection;
+
+import com.google.firebase.auth.FirebaseAuth
+
+public class firebaseAuth {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    
+}
