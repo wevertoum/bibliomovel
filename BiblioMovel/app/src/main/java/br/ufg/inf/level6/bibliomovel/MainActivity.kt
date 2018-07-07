777bba569fcbd2d7395da6209ef1a505b3265867
Erick Vieira <erick-vieira-s@outlook.com>
2018-07-07 15:50:20 -0300

    Página de login

--- BiblioMovel/app/src/main/java/br/ufg/inf/level6/bibliomovel/MainActivity.kt
+++ BiblioMovel/app/src/main/java/br/ufg/inf/level6/bibliomovel/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.ufg.inf.level6.bibliomovel
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
