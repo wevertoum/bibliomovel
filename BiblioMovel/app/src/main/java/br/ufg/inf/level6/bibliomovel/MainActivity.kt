59a0b3c792191bb1f70368daa1c31ef35da006da
Paula Resende <bellsuo@gmail.com>
2018-07-10 18:39:03 -0300

    Cadastro no firebase com e-mail e senha Verificação se campos estão devidamente preenchidos

--- BiblioMovel/app/src/main/java/br/ufg/inf/level6/bibliomovel/MainActivity.kt
+++ BiblioMovel/app/src/main/java/br/ufg/inf/level6/bibliomovel/MainActivity.kt
@@ -21,66 +21,20 @@ import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
-    var database = FirebaseDatabase.getInstance()
-    var myRef = database.getReference().child("livros")
-    var bookList = ArrayList<Livro>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Add event listener to "livros"
-        myRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                //add data livros to list
-                addDataToList(dataSnapshot)
-
-                var array = ArrayList<String>()
-
-                var adapter = BooksAdapter(this@MainActivity, bookList)
-                //ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, android.R.id.text1, array)
-                findViewById<ListView>(R.id.list_item).adapter = adapter
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.w("ERR", "Failed to read value.", error.toException())
-            }
-        })
-
-
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
 
-        val intent = Intent(this, ProfileActivity::class.java)
+        val intent = Intent(this, SignupActivity::class.java)
         startActivity(intent)
 
     }
 
-    private fun addDataToList(dataSnapshot: DataSnapshot) {
-        //get database items "Livros"
-        val items = dataSnapshot.children.iterator()
 
-        if(items !== null) {
-
-            while(items.hasNext()) {
-                var currentItem = items.next()
-                var livro = currentItem.getValue() as HashMap<Any, Any>
-                val book: Livro = Livro()
-
-                //Get properties
-                book.autor = livro.getValue("autor") as String
-                book.nome = livro.getValue("nome") as String
-                book.descricao = livro.getValue("descricao") as String
-                book.capa = livro.getValue("capa") as String
-
-                bookList.add(book)
-                Log.d("autor", "autor: " + book.autor)
-            }
         }
-    }
 
 
 
-}
+
+
